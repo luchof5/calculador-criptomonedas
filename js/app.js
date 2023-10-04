ccf5a217c1a634f91f9a58e75103d4633234482d
Luis Furtado <lucho_l.f@hotmail.com>
2023-10-04 19:35:01 -0300

    validacion formulario

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 // Crear un Promise
-const obtenerCriptomonedas = criptomeonedas => new Promise( resolve => {
-    resolve(criptomeonedas);
+const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 
 function consultarCriptomonedas() {
@@ -15,11 +27,11 @@ function consultarCriptomonedas() {
     fetch(url)
         .then( respuesta => respuesta.json() )
         .then( resultado => obtenerCriptomonedas(resultado.Data) )
-        .then( criptomeonedas => selectCriptomonedas(criptomeonedas) )
+        .then( criptomonedas => selectCriptomonedas(criptomonedas) )
 }
 
-function selectCriptomonedas(criptomeonedas) {
-    criptomeonedas.forEach(cripto => {
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
         const { FullName, Name } = cripto.CoinInfo;
 
         const option = document.createElement('option');
@@ -28,3 +40,40 @@ function selectCriptomonedas(criptomeonedas) {
         criptomonedasSelect.appendChild(option);
     });
 }
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if(moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return
+    }
+
+    
+}
+
+function mostrarAlerta(msg) {
+    const existeError = document.querySelector('.error');
+
+    if (!existeError) {
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('error');
+       
+        // Mensaje de error
+        divMensaje.textContent = msg;
+    
+        formulario.appendChild(divMensaje);
+    
+        setTimeout(() => {
+            divMensaje.remove()
+        }, 3000);
+    }
+}
+
